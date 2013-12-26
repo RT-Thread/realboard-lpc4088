ea7650ef7489eea73b98b9b912e095d9888d7614
bernard <bernard.xiong@gmail.com>
2013-12-26 08:08:16 +0800

    Add buildbot to build all of examples

---
+++ Software/scripts/buildbot.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+examples_dir = os.path.join('Software', 'examples')
+
+projects = os.listdir(examples_dir)
+for item in projects:
+    project_dir = os.path.join(examples_dir, item)
+    if os.path.isfile(os.path.join(project_dir, 'SConstruct')):
+        if os.system('scons --directory=' + project_dir) != 0:
+            print 'build failed!!'
+            break
