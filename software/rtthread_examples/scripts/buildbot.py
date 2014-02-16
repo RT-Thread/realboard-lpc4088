bfaee9c56e195b99f23236978a2bb66e50a36721
Bernard Xiong <bernard.xiong@gmail.com>
2014-02-16 16:08:07 +0800

    Add command for scons building

--- software/rtthread_examples/scripts/buildbot.py
+++ software/rtthread_examples/scripts/buildbot.py
@@ -3,10 +3,18 @@ import sys
 
 examples_dir = os.path.join('software', 'rtthread_examples', 'examples')
 
+target = ''
+if len(sys.argv) == 2:
+	target = sys.argv[1]
+
 projects = os.listdir(examples_dir)
 for item in projects:
     project_dir = os.path.join(examples_dir, item)
     if os.path.isfile(os.path.join(project_dir, 'SConstruct')):
-        if os.system('scons --directory=' + project_dir) != 0:
+    	command = 'scons ' + target
+    	command = command + ' --directory=' + project_dir
+
+    	print 'Building in Examples: ', item
+        if os.system(command) != 0:
             print 'build failed!!'
             break
