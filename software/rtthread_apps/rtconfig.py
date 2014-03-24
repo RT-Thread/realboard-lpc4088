0c712843be31a9979a18d6b6441de6182cb9effb
bernard <bernard.xiong@gmail.com>
2014-03-24 15:19:05 +0800

    Update UA building script.

--- software/rtthread_apps/rtconfig.py
+++ software/rtthread_apps/rtconfig.py
@@ -1,14 +1,17 @@
 import os
+cwd = os.path.split(os.path.realpath(__file__))[0]
 
 # RT-Thread root directory
 RTT_ROOT = r'Z:\github\rt-thread'
-
+BSP_ROOT = cwd + '/../rtthread_examples/examples/realboard'
 # toolchains
 EXEC_PATH = r'C:\Program Files (x86)\GNU Tools ARM Embedded\4.6 2012q2\bin'
 
-if os.getenv('RTT_EXEC_PATH'):
-    EXEC_PATH = os.getenv('RTT_EXEC_PATH')
+if os.getenv('RTT_ROOT'): RTT_ROOT = os.getenv('RTT_ROOT')
+if os.getenv('BSP_ROOT'): BSP_ROOT = os.getenv('BSP_ROOT')
+if os.getenv('RTT_EXEC_PATH'): EXEC_PATH = os.getenv('RTT_EXEC_PATH')
 
+PLATFORM = 'gcc'
 PREFIX = 'arm-none-eabi-'
 CC = PREFIX + 'gcc'
 CXX = PREFIX + 'g++'
