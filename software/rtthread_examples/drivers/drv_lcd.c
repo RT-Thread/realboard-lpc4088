2a5cfa4628d39a7c280d9ff2517a8f7a8486b083
xiaonong <nongxiaoming@gmail.com>
2014-03-25 20:35:24 +0800

    drv_lcd:modify lcd driver for use multiple buffer.

--- software/rtthread_examples/drivers/drv_lcd.c
+++ software/rtthread_examples/drivers/drv_lcd.c
@@ -9,10 +9,28 @@
 #include <lpc_pwm.h>
 #include <lpc_adc.h>
 
-//#define FRAME_BUFFER      0xA0000000
-static  rt_uint16_t *_rt_framebuffer = RT_NULL;
-#define RT_HW_LCD_WIDTH     480
-#define RT_HW_LCD_HEIGHT    272
+#define NUM_BUFFERS  2 // Number of multiple buffers to be used
+
+typedef enum 
+{
+ READY,
+ BUSY,
+ FINISH,
+} lcd_buf_status_t;
+typedef struct lcd_buffer
+{
+	lcd_buf_status_t status;
+	 /* framebuffer address and ops */
+ volatile rt_uint8_t *framebuffer_addr;
+ struct lcd_buffer *priv;
+ struct lcd_buffer *next;
+}lcd_buffer_t;
+
+static lcd_buffer_t lcd_buf[NUM_BUFFERS];
+static  rt_uint16_t * _rt_framebuffer = RT_NULL;
+static  lcd_buffer_t * current_buf = RT_NULL;
+#define RT_HW_LCD_WIDTH		480
+#define RT_HW_LCD_HEIGHT	272
 
 static struct rt_device_graphic_info _lcd_info;
 static struct rt_device  lcd;
@@ -30,156 +48,194 @@ static struct rt_device  lcd;
 
 #define LCD_PIX_CLK          (8*1000000UL)
 
-//#define LCD_CLK_PER_LINE     (LCD_H_SIZE + LCD_H_PULSE + LCD_H_FRONT_PORCH + LCD_H_BACK_PORCH)
-//#define LCD_LINES_PER_FRAME  (LCD_V_SIZE + LCD_V_PULSE + LCD_V_FRONT_PORCH + LCD_V_BACK_PORCH)
-//#define LCD_PIX_CLK          (LCD_CLK_PER_LINE * LCD_LINES_PER_FRAME)
+
 
 
 
 static void lcd_gpio_init(void)
 {
-    LPC_IOCON->P2_12  = 0x05; // 配置P2_12为VD3,  R0
-    LPC_IOCON->P2_6     = 0x07; // 配置P2_6为VD4,       R1
-    LPC_IOCON->P0_10    = 0x07; // 配置P0_10为VD5,      R2
-    LPC_IOCON->P4_28    = 0x05; // 配置P4_28为VD6,      R3
-    LPC_IOCON->P4_29 = 0x05;    // 配置P4_29为VD7,      R4
+  LPC_IOCON->P2_12 	= 0x05;	// 配置P2_12为VD3, 	R0
+	LPC_IOCON->P2_6 	= 0x07;	// 配置P2_6为VD4,		R1
+	LPC_IOCON->P0_10 	= 0x07;	// 配置P0_10为VD5,		R2
+	LPC_IOCON->P4_28	= 0x05;	// 配置P4_28为VD6,		R3
+	LPC_IOCON->P4_29 = 0x05;	// 配置P4_29为VD7,		R4
 
-    LPC_IOCON->P1_20    = 0x07; // 配置P1_20为VD10, G0
-    LPC_IOCON->P1_21    = 0x07; // 配置P1_21为VD11, G1
-    LPC_IOCON->P1_22    = 0x07; // 配置P1_22为VD12, G2
-    LPC_IOCON->P1_23    = 0x07; // 配置P1_23为VD13, G3
-    LPC_IOCON->P1_24    = 0x07; // 配置P1_24为VD14, G4
-    LPC_IOCON->P1_25    = 0x07; // 配置P1_25为 VD15,    G5
-
-    LPC_IOCON->P2_13    = 0x07; // 配置P2_13为VD19, B0
-    LPC_IOCON->P1_26 = 0x07;    // 配置P1_26为VD20, B1
-    LPC_IOCON->P1_27 = 0x07;    // 配置P1_27为VD21, B2
-    LPC_IOCON->P1_28 = 0x07;    // 配置P1_28为VD22, B3
-    LPC_IOCON->P1_29 = 0x07;    // 配置P1_29为VD23, B4
-
-    LPC_IOCON->P2_2 = 0x07;   // 配置P2_2为LCD的DCLK
-    LPC_IOCON->P2_0 = 0x07;   // 配置P2_0为LCD的DSIP(power)
-    LPC_IOCON->P2_5 = 0x07;   // 配置P2_5为LCD的HSYNC
-    LPC_IOCON->P2_3 = 0x07;   // 配置P2_3为LCD的VSYNC
-
-
-    LPC_IOCON->P2_1 = 0x07;   // 配置P2_1为LCD的背光控制GPIO
-    LPC_IOCON->P2_4 = 0x00;   // 配置P2_4为LCD的DataEn
-
-    LPC_GPIO2->DIR |= 1 << 4; //配置P2_4为输出
-    LPC_GPIO2->SET |= 1 << 4; //打开LCD背光
+	LPC_IOCON->P1_20	= 0x07;	// 配置P1_20为VD10,	G0
+	LPC_IOCON->P1_21	= 0x07;	// 配置P1_21为VD11,	G1
+	LPC_IOCON->P1_22	= 0x07;	// 配置P1_22为VD12,	G2
+	LPC_IOCON->P1_23	= 0x07;	// 配置P1_23为VD13,	G3
+	LPC_IOCON->P1_24	= 0x07;	// 配置P1_24为VD14,	G4
+	LPC_IOCON->P1_25	= 0x07;	// 配置P1_25为 VD15,	G5
+	
+	LPC_IOCON->P2_13	= 0x07;	// 配置P2_13为VD19,	B0
+	LPC_IOCON->P1_26 = 0x07;	// 配置P1_26为VD20,	B1
+	LPC_IOCON->P1_27 = 0x07;	// 配置P1_27为VD21,	B2
+	LPC_IOCON->P1_28 = 0x07;	// 配置P1_28为VD22,	B3
+	LPC_IOCON->P1_29 = 0x07;	// 配置P1_29为VD23,	B4
+	
+	LPC_IOCON->P2_2	= 0x07;	  // 配置P2_2为LCD的DCLK
+	LPC_IOCON->P2_0	= 0x07;	  // 配置P2_0为LCD的DSIP(power)
+	LPC_IOCON->P2_5	= 0x07;	  // 配置P2_5为LCD的HSYNC
+	LPC_IOCON->P2_3	= 0x07;	  // 配置P2_3为LCD的VSYNC
+	
+	
+	LPC_IOCON->P2_1	= 0x07;	  // 配置P2_1为LCD的背光控制GPIO
+	LPC_IOCON->P2_4	= 0x00;	  // 配置P2_4为LCD的DataEn
+	
+	LPC_GPIO2->DIR|=1<<4;     //配置P2_4为输出
+	LPC_GPIO2->SET|=1<<4;     //打开LCD背光
 }
 
-static rt_uint32_t find_clock_divisor(rt_uint32_t clock)
+static rt_uint32_t find_clock_divisor(rt_uint32_t clock) {
+  rt_uint32_t Divider;
+  rt_uint32_t r;
+
+  Divider = 1;
+  while (((SystemCoreClock / Divider) > clock) && (Divider <= 0x3F)) {
+    Divider++;
+  }
+  if (Divider <= 1) {
+    r = (1 << 26);  // Skip divider logic if clock divider is 1
+  } else {
+    Divider -= 2;
+    r = 0
+        | (((Divider >> 0) & 0x1F)
+        | (((Divider >> 5) & 0x1F) << 27))
+        ;
+  }
+  return r;
+}
+static void lcd_interrupt_enable(void)
 {
-    rt_uint32_t Divider;
-    rt_uint32_t r;
-
-    Divider = 1;
-    while (((SystemCoreClock / Divider) > clock) && (Divider <= 0x3F))
-    {
-        Divider++;
-    }
-    if (Divider <= 1)
-    {
-        r = (1 << 26);  // Skip divider logic if clock divider is 1
-    }
-    else
-    {
-        Divider -= 2;
-        r = 0
-            | (((Divider >> 0) & 0x1F)
-               | (((Divider >> 5) & 0x1F) << 27))
-            ;
-    }
-    return r;
+	LPC_LCD->INTCLR=0x04;
+	LPC_LCD->INTMSK |=0x04; 
+  NVIC_EnableIRQ(LCD_IRQn);
 }
+static void lcd_interrupt_disable(void)
+{
+  LPC_LCD->INTMSK &=~0x04; 
+  NVIC_DisableIRQ(LCD_IRQn);
+}
+void LCD_IRQHandler(void)
+{
+	rt_interrupt_enter();
+   if(LPC_LCD->INTSTAT&0x04)
+		 {
+			if(current_buf->next->status==FINISH)
+				{
+					LPC_LCD->UPBASE  =(rt_uint32_t)current_buf->next->framebuffer_addr;
+					current_buf=current_buf->next;
+					current_buf->next->status=READY;
+				//	rt_kprintf("buffer switch!\n");
+					lcd_interrupt_disable();
+        }
+			//清除帧同步中断标志位
+      LPC_LCD->INTCLR|=0x04;
+     }
+		rt_interrupt_leave();
+}
+
 /* RT-Thread Device Interface */
-static rt_err_t rt_lcd_init(rt_device_t dev)
+static rt_err_t rt_lcd_init (rt_device_t dev)
 {
 
-    /*LCD管脚配置*/
-    lcd_gpio_init();
-    /*打开LCD的控制时钟*/
-    LPC_SC ->PCONP |= 0x01;
-    /*禁止屏幕光标*/
-    LPC_LCD->CRSR_CTRL &= ~(1 << 0);
-    /*失能LCD控制器*/
-    LPC_LCD->CTRL &= ~(1UL << 0);
-    /*配置LCD工作在565模式*/
-    LPC_LCD->CTRL &= ~(0x07 < 1); //将寄存器的显示模式清零，CTRL的[3:1]位
-    LPC_LCD->CTRL |= (0x06 << 1); //565模式为110
-    /*设置LCD屏幕为TFT类型*/
-    LPC_LCD->CTRL |= (1 << 5);
-    /*设置LCD为单面板*/
-    LPC_LCD->CTRL &= ~(1 << 7);
-    /*设置为BGR*/
-    LPC_LCD->CTRL |= (1 << 8);
-    /*设置为小端字节序*/
-    LPC_LCD->CTRL &= ~(1 << 9);
-    // little endian pix order
-    LPC_LCD->CTRL &= ~(1 << 10);
-    // disable power
-    LPC_LCD->CTRL &= ~(1 << 11);
+	/*LCD管脚配置*/
+  lcd_gpio_init();
+	 /*打开LCD的控制时钟*/
+	LPC_SC ->PCONP |=0x01;
+	 /*禁止屏幕光标*/
+	LPC_LCD->CRSR_CTRL &=~(1<<0);
+	/*失能LCD控制器*/ 	
+	LPC_LCD->CTRL &= ~(1UL << 0); 
+	/*配置LCD工作在565模式*/
+	LPC_LCD->CTRL&=~(0x07<1);     //将寄存器的显示模式清零，CTRL的[3:1]位
+	LPC_LCD->CTRL|=(0x06<<1);     //565模式为110
+	/*设置LCD屏幕为TFT类型*/
+	LPC_LCD->CTRL |= (1<<5);
+	/*设置LCD为单面板*/
+	LPC_LCD->CTRL &= ~(1<<7);
+	/*设置为BGR*/ 
+	LPC_LCD->CTRL |= (1<<8);
+	/*设置为小端字节序*/
+	LPC_LCD->CTRL &= ~(1<<9);
+	// little endian pix order
+	LPC_LCD->CTRL &= ~(1<<10);
+	// disable power
+	LPC_LCD->CTRL &= ~(1<<11);
 
-    LPC_LCD->POL = (find_clock_divisor(LCD_PIX_CLK) <<  0);
-    // clock source for the LCD block is HCLK
-    LPC_LCD->POL &= ~(1 << 5);
-    // LCDFP pin is active LOW and inactive HIGH
-    LPC_LCD->POL |= (1 << 11);
-    // LCDLP pin is active LOW and inactive HIGH
-    LPC_LCD->POL |= (1 << 12);
-    // data is driven out into the LCD on the falling edge
-    LPC_LCD->POL &= ~(1 << 13);
-    // active high
-    LPC_LCD->POL &= ~(1 << 14);
-    LPC_LCD->POL &= ~(0x3FF << 16);
-    LPC_LCD->POL |= (LCD_H_SIZE - 1) << 16;
+	LPC_LCD->POL=(find_clock_divisor(LCD_PIX_CLK) <<  0);
+	// clock source for the LCD block is HCLK
+	LPC_LCD->POL &= ~(1<<5);
+	// LCDFP pin is active LOW and inactive HIGH
+	LPC_LCD->POL |= (1<<11);
+	// LCDLP pin is active LOW and inactive HIGH
+	LPC_LCD->POL |= (1<<12);
+	// data is driven out into the LCD on the falling edge
+	LPC_LCD->POL &= ~(1<<13);
+	// active high
+	LPC_LCD->POL &= ~(1<<14);
+	LPC_LCD->POL &= ~(0x3FF <<16);
+	LPC_LCD->POL |= (LCD_H_SIZE-1)<<16;
+	
+	//配置水平同步信号时序
+	LPC_LCD->TIMH = 0;                            //清空水平同步配置寄存器
+	LPC_LCD->TIMH |= (LCD_H_BACK_PORCH - 1)<<24;  //设置水平同步后端
+	LPC_LCD->TIMH |= (LCD_H_FRONT_PORCH - 1)<<16; //设置水平同步前端
+	LPC_LCD->TIMH |= (LCD_H_PULSE - 1)<<8;        //设置水平脉冲宽度
+	LPC_LCD->TIMH |= ((LCD_H_SIZE/16) - 1)<<2;    //设置水平像素点
+	
+	//配置垂直同步信号时序
+	LPC_LCD->TIMV = 0;  //reset TIMV value before setting
+	LPC_LCD->TIMV |= (LCD_V_BACK_PORCH)<<24;
+	LPC_LCD->TIMV |= (LCD_V_FRONT_PORCH)<<16;
+	LPC_LCD->TIMV |= (LCD_V_PULSE - 1)<<10;
+	LPC_LCD->TIMV |= LCD_V_SIZE - 1;
 
-    //配置水平同步信号时序
-    LPC_LCD->TIMH = 0;                            //清空水平同步配置寄存器
-    LPC_LCD->TIMH |= (LCD_H_BACK_PORCH - 1) << 24; //设置水平同步后端
-    LPC_LCD->TIMH |= (LCD_H_FRONT_PORCH - 1) << 16; //设置水平同步前端
-    LPC_LCD->TIMH |= (LCD_H_PULSE - 1) << 8;      //设置水平脉冲宽度
-    LPC_LCD->TIMH |= ((LCD_H_SIZE / 16) - 1) << 2; //设置水平像素点
+  LPC_SC->LCD_CFG = 0x0;  // No panel clock prescaler
 
-    //配置垂直同步信号时序
-    LPC_LCD->TIMV = 0;  //reset TIMV value before setting
-    LPC_LCD->TIMV |= (LCD_V_BACK_PORCH) << 24;
-    LPC_LCD->TIMV |= (LCD_V_FRONT_PORCH) << 16;
-    LPC_LCD->TIMV |= (LCD_V_PULSE - 1) << 10;
-    LPC_LCD->TIMV |= LCD_V_SIZE - 1;
+	NVIC_SetPriority(LCD_IRQn,((0x01 << 3) | 0x01));
+	
+	lcd_buf[0].framebuffer_addr = rt_malloc_align(sizeof(rt_uint16_t)*RT_HW_LCD_HEIGHT*RT_HW_LCD_WIDTH, 32);
+	lcd_buf[0].priv=&lcd_buf[1];
+	lcd_buf[0].next=&lcd_buf[1];
+	lcd_buf[1].framebuffer_addr = rt_malloc_align(sizeof(rt_uint16_t)*RT_HW_LCD_HEIGHT*RT_HW_LCD_WIDTH, 32);
+	lcd_buf[1].priv=&lcd_buf[0];
+	lcd_buf[1].next=&lcd_buf[0];
+	lcd_buf[0].status=FINISH;
+	lcd_buf[1].status=READY;
+	 current_buf=&lcd_buf[0];
+ LPC_LCD->UPBASE  =(rt_uint32_t)current_buf->framebuffer_addr;
 
-    ;
-    LPC_SC->LCD_CFG = 0x0;  // No panel clock prescaler
-    //
-    // Enable LCDC
-    //
-    LPC_LCD->UPBASE  = (rt_uint32_t)_rt_framebuffer;
-    LPC_LCD->CTRL   |= (1 <<  0);                 // Enable LCD signals
-    LPC_LCD->CTRL   |= (1 << 11);                 // Enable LCD power
-
-    return RT_EOK;
+  //
+  // Enable LCDC
+  //
+  LPC_LCD->CTRL   |= (1 <<  0);                 // Enable LCD signals
+  LPC_LCD->CTRL   |= (1 << 11);                 // Enable LCD power
+	return RT_EOK;
 }
 
-static rt_err_t rt_lcd_control(rt_device_t dev, rt_uint8_t cmd, void *args)
+static rt_err_t rt_lcd_control (rt_device_t dev, rt_uint8_t cmd, void *args)
 {
-    switch (cmd)
-    {
-    case RTGRAPHIC_CTRL_RECT_UPDATE:
-        break;
-    case RTGRAPHIC_CTRL_POWERON:
-        break;
-    case RTGRAPHIC_CTRL_POWEROFF:
-        break;
-    case RTGRAPHIC_CTRL_GET_INFO:
-        rt_memcpy(args, &_lcd_info, sizeof(_lcd_info));
-        break;
-    case RTGRAPHIC_CTRL_SET_MODE:
-        break;
-    }
+	switch (cmd)
+	{
+	case RTGRAPHIC_CTRL_RECT_UPDATE:
+		//while(current_buf->next->status!=READY);
+		rt_memcpy((void*)current_buf->next->framebuffer_addr,_rt_framebuffer,sizeof(rt_uint16_t)*RT_HW_LCD_HEIGHT*RT_HW_LCD_WIDTH);
+	  current_buf->next->status=FINISH;
+	  lcd_interrupt_enable();
+		break;
+	case RTGRAPHIC_CTRL_POWERON:
+		break;
+	case RTGRAPHIC_CTRL_POWEROFF:
+		break;
+	case RTGRAPHIC_CTRL_GET_INFO:
+		rt_memcpy(args, &_lcd_info, sizeof(_lcd_info));
+		break;
+	case RTGRAPHIC_CTRL_SET_MODE:
+		break;
+	}
 
-    return RT_EOK;
+	return RT_EOK;
 }
 
 
@@ -187,57 +243,63 @@ static rt_err_t rt_lcd_control(rt_device_t dev, rt_uint8_t cmd, void *args)
 
 void lcd_clear(rt_uint16_t color)
 {
-    volatile rt_uint16_t *p = (rt_uint16_t *)_lcd_info.framebuffer;
-    int x, y;
-
-    for (y = 0; y <= RT_HW_LCD_HEIGHT; y++)
-    {
-        for (x = 0; x <= RT_HW_LCD_WIDTH; x++)
-        {
-            *p++ = color; /* red */
-        }
-    }
+	volatile rt_uint16_t *p = (rt_uint16_t *)_lcd_info.framebuffer;
+	int x, y;
+//	char *buf = rt_malloc_align(sizeof(rt_uint16_t)*RT_HW_LCD_HEIGHT*RT_HW_LCD_WIDTH, 32);
+//	rt_memset(buf,0x55,RT_HW_LCD_HEIGHT*RT_HW_LCD_WIDTH*2);
+	
+	for (y = 0; y <= RT_HW_LCD_HEIGHT; y++)
+	{
+		for (x = 0; x <= RT_HW_LCD_WIDTH; x++)
+		{
+			*p++ =color;  /* red */
+		}
+	}
 }
 
 void rt_hw_lcd_init(void)
 {
 
 
-    _rt_framebuffer = rt_malloc_align(sizeof(rt_uint16_t) * RT_HW_LCD_HEIGHT * RT_HW_LCD_WIDTH, 32);
-    if (_rt_framebuffer == RT_NULL) return; /* no memory yet */
-    //_rt_framebuffer = (rt_uint16_t *)FRAME_BUFFER;
-    _lcd_info.bits_per_pixel = 16;
-    _lcd_info.pixel_format = RTGRAPHIC_PIXEL_FORMAT_RGB565;
-    _lcd_info.framebuffer = (void *)_rt_framebuffer;
-    _lcd_info.width = RT_HW_LCD_WIDTH;
-    _lcd_info.height = RT_HW_LCD_HEIGHT;
+	_rt_framebuffer = rt_malloc_align(sizeof(rt_uint16_t)*RT_HW_LCD_HEIGHT*RT_HW_LCD_WIDTH, 32);
+  if (_rt_framebuffer == RT_NULL) return; /* no memory yet */
+  //_rt_framebuffer = (rt_uint16_t *)FRAME_BUFFER;
+	_lcd_info.bits_per_pixel = 16;
+	_lcd_info.pixel_format = RTGRAPHIC_PIXEL_FORMAT_RGB565;
+	_lcd_info.framebuffer = (void*)_rt_framebuffer;
+	_lcd_info.width = RT_HW_LCD_WIDTH;
+	_lcd_info.height = RT_HW_LCD_HEIGHT;
+  
+	/* init device structure */
+	lcd.type = RT_Device_Class_Graphic;
+	lcd.init = rt_lcd_init;
+	lcd.open = RT_NULL;
+	lcd.close = RT_NULL;
+	lcd.control = rt_lcd_control;
+	lcd.user_data = (void*)&_lcd_info;
 
-    /* init device structure */
-    lcd.type = RT_Device_Class_Graphic;
-    lcd.init = rt_lcd_init;
-    lcd.open = RT_NULL;
-    lcd.close = RT_NULL;
-    lcd.control = rt_lcd_control;
-    lcd.user_data = (void *)&_lcd_info;
-
-    /* register lcd device to RT-Thread */
-    rt_device_register(&lcd, "lcd", RT_DEVICE_FLAG_RDWR);
-
-    rt_lcd_init(&lcd);
+	/* register lcd device to RT-Thread */
+	rt_device_register(&lcd, "lcd", RT_DEVICE_FLAG_RDWR);
+	
+	rt_lcd_init(&lcd);
 }
 
 
 void lcd_test(void)
 {
-    lcd_clear(0xf800);
-    rt_thread_delay(200);
-    lcd_clear(0x07e0);
-    rt_thread_delay(200);
-    lcd_clear(0x001f);
-    rt_thread_delay(200);
+  lcd_clear(0xf800);
+	rt_thread_delay(200);
+	lcd_clear(0x07e0);
+	rt_thread_delay(200);
+	lcd_clear(0x001f);
+	rt_thread_delay(200);
 }
 #ifdef RT_USING_FINSH
 #include <finsh.h>
-FINSH_FUNCTION_EXPORT(lcd_clear, lcd_clear);
-FINSH_FUNCTION_EXPORT(lcd_test, lcd_test);
+FINSH_FUNCTION_EXPORT(lcd_clear, lcd_clear  );
+FINSH_FUNCTION_EXPORT(lcd_test, lcd_test  );
+#ifdef FINSH_USING_MSH
+MSH_CMD_EXPORT(lcd_clear, lcd_clear);
+MSH_CMD_EXPORT(lcd_test, lcd_test);
+#endif
 #endif
