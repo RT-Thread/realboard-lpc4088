97c328aed9f56e840b37f12cdd8b2c69972ae27d
bernard <bernard.xiong@gmail.com>
2015-01-25 23:37:58 +0800

    change the number of argument of mountnfs function.

--- software/rtthread_examples/examples/realboard/applications/mnt_nfs.c
+++ software/rtthread_examples/examples/realboard/applications/mnt_nfs.c
@@ -1,12 +1,12 @@
 #include <components.h>
 
-int mountnfs(const char * path)
+int mountnfs(const char * path, const char* export_dir)
 {
     const char * mountpath = "/";
     if (path != NULL) mountpath = path;
 
-    rt_kprintf("mount nfs to %s...", mountpath);
-    if (dfs_mount(RT_NULL, mountpath, "nfs", 0, RT_NFS_HOST_EXPORT) == 0)
+    rt_kprintf("mount nfs:%s to %s...", export_dir, mountpath);
+    if (dfs_mount(RT_NULL, mountpath, "nfs", 0, export_dir) == 0)
     {
         rt_kprintf("[ok]\n");
         return 0;
@@ -22,12 +22,12 @@ FINSH_FUNCTION_EXPORT(mountnfs, mount nfs);
 /* mountnfs, exported to msh */
 int cmd_mountnfs(int argc, char** argv)
 {
-    if (argc == 1)
+    if (argc < 3)
     {
-        rt_kprintf("%s path\n", argv[0]);
+        rt_kprintf("Usage:%s remote_export path\n", argv[0]);
         return 0;
     }
 
-    return mountnfs(argv[1]);
+    return mountnfs(argv[1], argv[2]);
 }
 FINSH_FUNCTION_EXPORT_ALIAS(cmd_mountnfs, __cmd_mountnfs, mount NFS file system.);
