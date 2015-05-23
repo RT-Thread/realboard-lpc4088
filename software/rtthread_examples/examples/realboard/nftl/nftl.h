c6377d879345ab7867b7bac7247bccbd5311e833
Bernard Xiong <bernard.xiong@gmail.com>
2015-05-23 20:43:06 +0800

    Update NFTL to 1.0.17 version

--- software/rtthread_examples/examples/realboard/nftl/nftl.h
+++ software/rtthread_examples/examples/realboard/nftl/nftl.h
@@ -12,8 +12,10 @@
  * 2013-06-10     Bernard      Enable backup mapping block
  * 2013-09-18     Bernard      Fix the quick search issue in mapping table initialization.
  * 2013-11-11     RealThread   1.0.13 version which supports none-os environment.
- * 2013-12-25     RealThread   1.0.14 version which optimize memory/stack usage.
- * 2014-01-20     RealThread   1.0.15 version which optimize the scan speed in boot.
+ * 2013-12-25     RealThread   1.0.14 version which optimizes for memory/stack usage.
+ * 2014-01-20     RealThread   1.0.15 version which optimizes for the scan speed in boot.
+ * 2015-01-05     RealThread   1.0.16 version which addes NFTL manargement block protect with MPU.
+ * 2015-02-12     RealThread   1.0.17 version which fix the mapping block wear_leveling issue.
  */
 
 #ifndef __NAND_FTL_H__
@@ -25,7 +27,7 @@
 /* NFTL version information */
 #define NFTL_VERSION                    1L              /* major version number */
 #define NFTL_SUBVERSION                 0L              /* minor version number */
-#define NFTL_REVISION                   15L             /* revise version number */
+#define NFTL_REVISION                   17L             /* revise version number */
 
 /* NFTL options */
 #define NFTL_BLOCKS_MAX                 512             /* only 512 block for evaluation */
@@ -46,6 +48,9 @@
 /* using static memory for NFTl layer */
 // #define NFTL_USING_STATIC
 
+/* whether using MPU to protect NFTL core data */
+// #define NFTL_USING_MPU
+
 #define NFTL_MALLOC(sz)					RT_KERNEL_MALLOC(sz)
 #define NFTL_FREE(ptr)					RT_KERNEL_FREE(ptr)
 
@@ -54,28 +59,28 @@ rt_err_t nftl_attach(const char* mtd_device);
 
 /* NFTL API for diskio */
 rt_err_t nftl_read_page(struct rt_mtd_nand_device *device,
-	rt_uint16_t                block_offset,
-	rt_uint16_t                page_offset,
-	rt_uint8_t                *buffer);
+                        rt_uint16_t                block_offset,
+                        rt_uint16_t                page_offset,
+                        rt_uint8_t                *buffer);
 rt_err_t nftl_write_page(struct rt_mtd_nand_device *device,
-	rt_uint16_t                block_offset,
-	rt_uint16_t                page_offset,
-	const rt_uint8_t          *buffer);
+                         rt_uint16_t                block_offset,
+                         rt_uint16_t                page_offset,
+                         const rt_uint8_t          *buffer);
 
 rt_err_t nftl_read_multi_page(struct rt_mtd_nand_device *device,
-	rt_uint16_t                block_offset,
-	rt_uint16_t                page_offset,
-	rt_uint8_t                *buffer,
-	rt_size_t                  count);
+                              rt_uint16_t                block_offset,
+                              rt_uint16_t                page_offset,
+                              rt_uint8_t                *buffer,
+                              rt_size_t                  count);
 rt_err_t nftl_write_multi_page(struct rt_mtd_nand_device *device,
-	rt_uint16_t                block_offset,
-	rt_uint16_t                page_offset,
-	const rt_uint8_t          *buffer,
-	rt_size_t                  count);
+                               rt_uint16_t                block_offset,
+                               rt_uint16_t                page_offset,
+                               const rt_uint8_t          *buffer,
+                               rt_size_t                  count);
 
 rt_err_t nftl_erase_pages(struct rt_mtd_nand_device* device, rt_uint32_t page_begin, rt_uint32_t page_end);
 
-rt_err_t nftl_mapping_flush(struct rt_mtd_nand_device* device);
+rt_err_t nftl_mapping_flush(struct rt_mtd_nand_device* device, rt_bool_t protect);
 
 rt_err_t nftl_layer_init(struct rt_mtd_nand_device* device);
 
